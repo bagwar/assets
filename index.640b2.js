71caa0fb1aa904cb15bb3acdc47504728ed9526a
alesandrshy <alesandrshyti454@gmail.com>
2025-01-09 08:59:17 +0000

    deploy V0.4.3

--- index.640b2.js
+++ index.640b2.js
@@ -1,4 +1,4 @@
-System.register(["./application.776ad.js"], function (_export, _context) {
+System.register(["./application.8eeaa.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
