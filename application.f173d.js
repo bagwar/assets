1a5936ea14cdd97e5a6ce5410e6777ea1fcc3ef0
alesandrshy <alesandrshyti454@gmail.com>
2025-01-15 06:44:21 +0000

    deploy V0.4.10

--- application.f173d.js
+++ application.f173d.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.62331.json';
+          this.settingsPath = 'src/settings.52612.json';
           this.showFPS = false;
         }
         init(engine) {
--- index.a22cf.js
+++ index.a22cf.js
@@ -1,4 +1,4 @@
-System.register(["./application.cc77b.js"], function (_export, _context) {
+System.register(["./application.f173d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
