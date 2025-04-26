a97410ac9bd3a3b3b48d9db2c72028b136caa9ac
alesandrshy <alesandrshyti454@gmail.com>
2025-04-26 03:14:25 +0000

    deploy V0.7.3.1

--- index.cb96c.js
+++ index.cb96c.js
@@ -1,4 +1,4 @@
-System.register(["./application.f6c80.js"], function (_export, _context) {
+System.register(["./application.c219a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
